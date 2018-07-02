417dc5df87b623406a6c6e0b325a52eae8785531
Jag <chadha@toechtersoehne.com>
2018-07-02 15:54:27 +0200

    [AOK-292] adding google fonts locally

--- package.js
+++ package.js
@@ -7,7 +7,7 @@
 
 Package.describe({
   name: 'toechterundsoehne:mfactory-admin-lte',
-  version: '1.0.1',
+  version: '1.0.2',
   summary: 'Mfactory\'s AdminLTE dashboard theme (unpublished tip of offical github repo)',
   git: 'https://github.com/toechtersoehne/mfactory-admin-lte',
   documentation: 'README.md'
@@ -21,10 +21,32 @@ Package.onUse(function(api) {
     'reactive-var'
   ], 'client');
 
+
   api.addFiles([
     'admin-lte.html',
     'admin-lte.js'
   ], 'client');
+
+//2018-07-02-jc adding google fonts locally [AOK-292]
+//reference via /packages/toechterundsoehne_mfactory-admin-lte/fonts/<font-name>.woff
+  api.addFiles([
+    'fonts/source-sans-pro-v11-latin-300.woff2',
+    'fonts/source-sans-pro-v11-latin-300.woff',
+    'fonts/source-sans-pro-v11-latin-300italic.woff2',
+    'fonts/source-sans-pro-v11-latin-300italic.woff',
+    'fonts/source-sans-pro-v11-latin-regular.woff2',
+    'fonts/source-sans-pro-v11-latin-regular.woff',
+    'fonts/source-sans-pro-v11-latin-italic.woff2',
+    'fonts/source-sans-pro-v11-latin-italic.woff',
+    'fonts/source-sans-pro-v11-latin-600.woff2',
+    'fonts/source-sans-pro-v11-latin-600.woff',
+    'fonts/source-sans-pro-v11-latin-600italic.woff2',
+    'fonts/source-sans-pro-v11-latin-600italic.woff',
+    'fonts/source-sans-pro-v11-latin-700.woff2',
+    'fonts/source-sans-pro-v11-latin-700.woff'
+  ], 'client', {
+    isAsset: true
+});
 
   api.addFiles([
     'css/AdminLTE.min.css',
